19d15cab736152c1e5146b637eae41ddeb9e6cf9
CyberS <stservais0409@gmail.com>
2025-05-16 18:36:03 +0200

    Ajout du module lateral.py

---
+++ core/lateral.py
@@ -0,0 +1,4 @@
+def run(user):
+    print(f"[*] Simulation de mouvement latéral avec l'utilisateur : {user}")
+    print("[+] (Démo) Tentative d'accès distant simulée...")
+    # Ici on pourrait ajouter : RDP, WMI, WinRM, etc.
